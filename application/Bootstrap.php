e250d1553fdf3a0e5ea092ad19c11e49a32f8b20
Alexander M. Turek <me@derrabus.de>
2019-09-24 15:00:28 +0200

    Wrap Symfony around the old application.

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,15 +2,6 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
-    protected function _initErrorHandler()
-    {
-        set_error_handler(static function ($severity, $message, $file, $line) {
-            if (error_reporting() & $severity) {
-                throw new ErrorException($message, 0, $severity, $file, $line);
-            }
-        });
-    }
-
     protected function _initTranslate()
     {
         Zend_Registry::set('Zend_Locale', new Zend_Locale('de_DE'));
--- public/index.php
+++ public/index.php
@@ -1,28 +1,9 @@
 <?php
 
-use App\Kernel;
+use Symfony\Component\HttpFoundation\Response;
+use TravelOrganizer\Kernel;
 use Symfony\Component\Debug\Debug;
 use Symfony\Component\HttpFoundation\Request;
-
-require dirname(__DIR__).'/vendor/autoload.php';
-
-// Define path to application directory
-defined('APPLICATION_PATH')
-    || define('APPLICATION_PATH', realpath(dirname(__FILE__).'/../application'));
-
-// Define application environment
-defined('APPLICATION_ENV')
-    || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
-
-// Create application, bootstrap, and run
-$application = new Zend_Application(
-    APPLICATION_ENV,
-    APPLICATION_PATH.'/configs/application.ini'
-);
-$application->bootstrap()
-    ->run();
-
-exit;
 
 require dirname(__DIR__).'/config/bootstrap.php';
 
@@ -40,8 +21,31 @@ if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? $_ENV['TRUSTED_HOSTS'] ?? false
     Request::setTrustedHosts([$trustedHosts]);
 }
 
+// Define path to application directory
+defined('APPLICATION_PATH')
+    || define('APPLICATION_PATH', realpath(dirname(__FILE__).'/../application'));
+
+// Define application environment
+defined('APPLICATION_ENV')
+    || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
+
 $kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
+$kernel->boot();
+
+// Create application, bootstrap, and run
+$application = new Zend_Application(
+    APPLICATION_ENV,
+    APPLICATION_PATH.'/configs/application.ini'
+);
+$application->bootstrap();
+
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
-$response->send();
+
+if (Response::HTTP_NOT_FOUND === $response->getStatusCode() || Response::HTTP_METHOD_NOT_ALLOWED === $response->getStatusCode()) {
+    $application->run();
+} else {
+    $response->send();
+}
+
 $kernel->terminate($request, $response);
