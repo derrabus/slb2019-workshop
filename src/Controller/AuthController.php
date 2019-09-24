ba95f20f6b20e12d6f7ff29fdf35b31f12f7e33a
Alexander M. Turek <me@derrabus.de>
2019-09-24 16:13:59 +0200

    AuthController in Symfony.

---
+++ src/Controller/AuthController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace TravelOrganizer\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+final class AuthController extends AbstractController
+{
+    private $translate;
+
+    public function __construct(\Zend_Translate $translate)
+    {
+        $this->translate = $translate;
+    }
+
+    /**
+     * @Route("/auth/login", name="auth_login", methods={"GET", "POST"})
+     */
+    public function loginAction(Request $request): Response
+    {
+        if (\Zend_Auth::getInstance()->hasIdentity()) {
+            return $this->redirect('/');
+        }
+
+        $error = null;
+        $form = new \Application_Form_Login();
+
+        if ($request->isMethod(Request::METHOD_POST) && $form->isValid($request->request->all())) {
+            $adapter = new \TravelOrganizer_Auth_DbTableAdapter(
+                $form->getValue('username'),
+                $form->getValue('password')
+            );
+
+            $auth = \Zend_Auth::getInstance();
+            $result = $auth->authenticate($adapter);
+
+            if ($result->isValid()) {
+                return $this->redirect('/');
+            }
+
+            $error = implode("\n", array_map([$this->translate, 'translate'], $result->getMessages()));
+        }
+
+        return $this->render('login.html.twig', [
+            'form' => $form,
+            'error' => $error,
+        ]);
+    }
+
+    /**
+     * @Route("/auth/logout", name="auth_logout", methods={"GET"})
+     */
+    public function logoutAction(): Response
+    {
+        \Zend_Auth::getInstance()->clearIdentity();
+
+        return $this->redirectToRoute('auth_login');
+    }
+}
